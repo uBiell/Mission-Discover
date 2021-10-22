da9dc6b3736219e397d6fb1dc00b61a31d6b8544
Gabriel dos Santos Almeida <obiellalm@gmail.com>
2021-10-21 21:25:15 -0300

    Project Finished

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -5,16 +5,48 @@ module.exports = {
         const db = await Database()
         const pass = req.body.password
         let roomId
+        let isRoom = true
 
-        for(var i = 0; i < 6; i++){
-            i === 0 ? roomId = Math.floor(Math.random() * 10).toString() : roomId += Math.floor(Math.random() * 10).toString()
+        while(isRoom){
+            //Generate the room number
+            for(var i = 0; i < 6; i++){
+                i === 0 ? roomId = Math.floor(Math.random() * 10).toString() : roomId += Math.floor(Math.random() * 10).toString()
+            }
+            // Verify if number already exists
+            const roomsExistIds = await db.all(`SELECT id FROM rooms`)
+            isRoom = roomsExistIds.some(roomExistId => roomExistId === roomId)
+            
+            if(!isRoom){
+                //Insert the room in db
+                await db.run(`INSERT INTO rooms (id, pass) 
+                VALUES (${parseInt(roomId)}, ${pass})`)
+            }
         }
-
-        await db.run(`INSERT INTO rooms (id, pass) 
-        VALUES (${parseInt(roomId)}, ${pass})`)
-        
+    
         db.close()
 
         res.redirect(`/room/${roomId}`)        
+    },
+
+    async open(req, res){
+        const db = await Database()
+        const roomId = req.params.room
+        const questions = await db.all(`SELECT * FROM questions WHERE room = ${roomId} and read = 0`)
+        const questionsRead = await db.all(`SELECT * FROM questions WHERE room = ${roomId} and read = 1`)
+        let isNoQuestions
+
+        if (questions.length === 0){
+            if(questionsRead.length === 0){
+                isNoQuestions = true
+            }
+        }
+        
+        res.render("room", {roomId: roomId, questions: questions, questionsRead: questionsRead, isNoQuestions: isNoQuestions})
+    },
+
+    async enter(req, res){
+        const roomId = req.body.roomId
+
+        res.redirect(`/room/${roomId}`)
     }
 }
